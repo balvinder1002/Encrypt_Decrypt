f2b2359583d60274dcf001b236493940fe86754a
balvinder1002 <53038544+balvinder1002@users.noreply.github.com>
2019-08-03 12:40:03 +0530

    just added decryption and the option for it

--- Encrypt_Decrypt.py
+++ Encrypt_Decrypt.py
@@ -1,17 +1,48 @@
-my_dict = {"A": "E", "B": "F", "C": "G", "D": "H", "E": "I", "F": "J", "G": "K",
+en_dict = {"A": "E", "B": "F", "C": "G", "D": "H", "E": "I", "F": "J", "G": "K",
            "H": "L", "I": "M", "J": "N", "K": "O", "L": "P", "M": "Q", "N": "R",
            "O": "S", "P": "T", "Q": "U", "R": "V", "S": "W", "T": "X", "U": "Y",
            "V": "Z", "W": "A", "X": "B", "Y": "C", "Z": "D", "1": "4", "2": "5",
            "3": "6", "4": "7", "5": "8", "6": "9", "7": "0", "8": "1", "9": "2",
            "0": "3", " ": " "}
 
-message = input("Enter Message: ").upper()
-encrypted = ""
+de_dict = {"E": "A", "F": "B", "G": "C", "H": "D", "I": "E", "J": "F", "K": "G",
+           "L": "H", "M": "I", "N": "J", "O": "K", "P": "L", "Q": "M", "R": "N",
+           "S": "O", "T": "P", "U": "Q", "V": "R", "W": "S", "X": "T", "Y": "U",
+           "Z": "V", "A": "Q", "B": "X", "C": "Y", "D": "Z", "4": "1", "5": "2",
+           "6": "3", "7": "4", "8": "5", "9": "6", "0": "7", "1": "8", "2": "9",
+           "3": "0", " ": " "}
 
-for letters in message:
-    if letters in my_dict:
-        encrypted += my_dict[letters]
-    else:
-        encrypted += letters
 
-print(encrypted.lower())
+def encrypt():
+    message = input("Enter Message for Encryption: ").upper()
+    encrypted = ""
+
+    for letters in message:
+        if letters in en_dict:
+            encrypted += en_dict[letters]
+        else:
+            encrypted += letters
+
+    print(encrypted.lower())
+
+
+def decrypt():
+    message = input("Enter Message for Decryption: ").upper()
+    decrypted = ""
+
+    for letters in message:
+        if letters in de_dict:
+            decrypted += de_dict[letters]
+        else:
+            decrypted += letters
+
+    print(decrypted.lower())
+
+
+option = int(input("Enter your Choice:\n1.Encryption\n2.Decryption\n"))
+if option == 1:
+    encrypt()
+elif option == 2:
+    decrypt()
+else:
+    print("Wrong Choice")
